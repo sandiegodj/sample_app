c9eeb1f2f1d04483eeb1c757d121d2693afcd7ca
sandiegodj <davidjanczyn@gmail.com>
2014-09-19 14:00:15 -0700

    Finish sign in

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -55,9 +55,9 @@ describe "Static pages" do
     click_link "Contact"
     expect(page).to have_title(full_title('Contact'))
     click_link "Home"
+
     click_link "Sign up now!"
     expect(page).to have_title(full_title('Sign up'))
     click_link "sample app"
-    expect(page).to have_title(full_title('Ruby on Rails Tutorial Twitter Clone'))
   end
 end
